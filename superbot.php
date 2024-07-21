f2d2cdb30f75f0ac49f045d945d340981a5fd26c
SHIMANTA DAS <iamshimantadas@gmail.com>
2024-07-22 00:31:18 +0530

    first commit

---
+++ superbot.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       SuperBot
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       This is a bot assistant, which also powered by Google Gemini API. It can reply against queries 24x7 to website visitors.
+ * Version:           1.1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shimanta Das
+ * Author URI:        https://microcodes.in/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       chatbot
+ */
+?>
